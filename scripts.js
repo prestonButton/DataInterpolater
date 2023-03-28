5e6794c0bf71a59177b8fe368d4bd5bdc85d2099
Preston Button <98365800+prestonButton@users.noreply.github.com>
2023-03-28 10:59:31 -0400

    Update files

--- scripts.js
+++ scripts.js
@@ -4,22 +4,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const fileNameInput = document.getElementById("file-name");
   const result = document.getElementById("result");
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-    const data = dataInput.value.split(",").map(Number);
-    const interpolatedData = interpolateData(data, 80);
-    const sortedData = interpolatedData.sort((a, b) => a - b);
-    const csvData = convertToCSV(sortedData);
+    const dataInputValue = document.getElementById("data-input").value;
+    const dataPoints = dataInputValue
+      .split(/,|\s/)
+      .map((point) => parseFloat(point.trim()))
+      .filter((point) => !isNaN(point));
 
-    const fileName = `${fileNameInput.value}.csv`;
-    createDownloadLink(csvData, fileName, "text/csv");
+    const interpolatedData = interpolateData(dataPoints, 80);
+    const fileName =
+      document.getElementById("file-name-input").value || "interpolated_data";
+
+    createCSVDownload(interpolatedData, fileName);
+    createTrendlineSVG(interpolatedData, fileName);
   });
 });
-
-function lerp(a, b, t) {
-  return a + (b - a) * t;
-}
 
 function interpolateData(data, targetLength) {
   if (data.length < 2) {
@@ -45,30 +46,170 @@ function interpolateData(data, targetLength) {
     }
   }
 
+  interpolatedData[targetLength - 1] = data[data.length - 1];
   return interpolatedData;
 }
 
-function convertToCSV(data) {
-  const header = "Index,Data\n";
-  const rows = data.map((value, index) => `${index + 1},${value}`).join("\n");
-  return header + rows;
+function lerp(a, b, t) {
+  return a + (b - a) * t;
 }
 
-function createDownloadLink(fileData, fileName, mimeType) {
+function createCSVDownload(data, fileName) {
+  const csvData = data
+    .map((value, index) => `${index + 1},${value}`)
+    .join("\n");
+  const csvHeader = "Index,Data\n";
+  const csvContent = csvHeader + csvData;
+
+  const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+  const url = URL.createObjectURL(blob);
+
+  createDownloadLink(
+    url,
+    fileName + ".csv",
+    "text/csv;charset=utf-8;",
+    "csv-link",
+    "Download CSV"
+  );
+}
+
+function createTrendlineSVG(data, fileName) {
+  const svgWidth = 800;
+  const svgHeight = 400;
+  const padding = 40;
+
+  const maxValue = Math.max(...data) || 1;
+  const xScale = (index) =>
+    (index / (data.length - 1)) * (svgWidth - 2 * padding) + padding;
+  const yScale = (value) =>
+    svgHeight - padding - (value / maxValue) * (svgHeight - 2 * padding);
+
+  const polylinePoints = data
+    .map((value, index) => `${xScale(index)},${yScale(value)}`)
+    .join(" ");
+
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("width", svgWidth);
+  svg.setAttribute("height", svgHeight);
+
+  const defs = document.createElementNS("http://www.w3.org/2000/svg", "defs");
+  const gradient = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "linearGradient"
+  );
+  gradient.setAttribute("id", "gradient");
+  gradient.setAttribute("x1", "0%");
+  gradient.setAttribute("y1", "0%");
+  gradient.setAttribute("x2", "0%");
+  gradient.setAttribute("y2", "100%");
+
+  const stop1 = document.createElementNS("http://www.w3.org/2000/svg", "stop");
+  stop1.setAttribute("offset", "0%");
+  stop1.setAttribute("stop-color", "#272A32");
+  stop1.setAttribute("stop-opacity", "0.6");
+
+  const stop2 = document.createElementNS("http://www.w3.org/2000/svg", "stop");
+  stop2.setAttribute("offset", "100%");
+  stop2.setAttribute("stop-color", "#272A32");
+  stop2.setAttribute("stop-opacity", "0");
+
+  gradient.appendChild(stop1);
+  gradient.appendChild(stop2);
+  defs.appendChild(gradient);
+  svg.appendChild(defs);
+
+  const polyline = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "polyline"
+  );
+  polyline.setAttribute("points", polylinePoints);
+  polyline.setAttribute("fill", "none");
+  polyline.setAttribute("stroke", "#272A32");
+  polyline.setAttribute("stroke-width", 3);
+
+  svg.appendChild(polyline);
+
+  const polygonPoints =
+    polylinePoints +
+    ` ${svgWidth - padding},${svgHeight - padding} ${padding},${
+      svgHeight - padding
+    }`;
+  const polygon = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "polygon"
+  );
+  polygon.setAttribute("points", polygonPoints);
+  polygon.setAttribute("fill", "url(#gradient)");
+
+  svg.insertBefore(polygon, polyline);
+
+  const existingSVG = document.getElementById("trendline-svg");
+  if (existingSVG) {
+    existingSVG.remove();
+  }
+
+  svg.id = "trendline-svg";
+  result.appendChild(svg);
+
+  const svgData = new XMLSerializer().serializeToString(svg);
+  const svgBlob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
+  const svgUrl = URL.createObjectURL(svgBlob);
+  const svgFileName = fileName + ".svg";
+  createDownloadLink(
+    svgUrl,
+    svgFileName,
+    "image/svg+xml;charset=utf-8",
+    "svg-link",
+    "Download SVG"
+  );
+
+  createCopyButton("Copy SVG", () => {
+    navigator.clipboard.writeText(svgData).then(
+      () => {
+        console.log("SVG copied to clipboard");
+      },
+      (err) => {
+        console.error("Could not copy SVG to clipboard", err);
+      }
+    );
+  });
+}
+
+function createCopyButton(buttonText, onClick) {
+  const button = document.createElement("button");
+  button.textContent = buttonText;
+  button.addEventListener("click", onClick);
+
+  const existingButton = document.getElementById("copy-button");
+  if (existingButton) {
+    existingButton.remove();
+  }
+
+  button.id = "copy-button";
+  result.appendChild(button);
+}
+
+
+
+function createDownloadLink(fileData, fileName, mimeType, linkId, linkText) {
   const downloadLink = document.createElement("a");
-  const fileBlob = new Blob([fileData], { type: mimeType });
-  const url = URL.createObjectURL(fileBlob);
-
-  downloadLink.href = url;
+  downloadLink.href = fileData;
   downloadLink.download = fileName;
-  downloadLink.textContent = `Download ${fileName}`;
+  downloadLink.textContent = linkText;
 
-  // Remove any existing download link
-  const existingLink = document.getElementById("download-link");
+  if (mimeType === "image/svg+xml;charset=utf-8") {
+    downloadLink.addEventListener("click", (event) => {
+      setTimeout(() => {
+        URL.revokeObjectURL(fileData);
+      }, 1000);
+    });
+  }
+
+  const existingLink = document.getElementById(linkId);
   if (existingLink) {
     existingLink.remove();
   }
 
-  downloadLink.id = "download-link";
+  downloadLink.id = linkId;
   result.appendChild(downloadLink);
 }
